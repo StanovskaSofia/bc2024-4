c2459cfccf5611c78da748cd810478d92a476a46
StanovskaSofia bc2024-4 <stanovska.sofia@gmail.com>
2024-10-26 21:49:44 +0300

    коміт одного методу

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
+const { promises: fs } = require('fs');
 const { Command } = require('commander');
+const path = require('path');
 const program = new Command();
 
 program
@@ -8,15 +10,26 @@ program
   .requiredOption('-c, --cache <path>', 'cache directory');
 
 program.parse(process.argv);
+const { host, port, cache } = program.opts();
 
-const options = program.opts();
+const getFilePath = (code) => path.join(cache, `${code}.jpg`);
 
-const server = http.createServer((req, res) => 
-{
- res.end('Hello, World!\n');
+const server = http.createServer(async (req, res) => {
+  const code = req.url.slice(1);
+  const filePath = getFilePath(code);
+
+  if (req.method === 'GET') {
+    try {
+      const data = await fs.readFile(filePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(data);
+    } catch (error) {
+      res.writeHead(404);
+      res.end('Not Found\n');
+    }
+  } 
 });
 
-server.listen(options.port, options.host, () => 
-{
-  console.log(`server running at http://${options.host}:${options.port}/`);
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
 });
